23a76a58507689f9247a8f69c5d6cb8f6e648189
cool-pants <muban.sg@gmail.com>
2024-04-26 20:13:17 +0530

    feat: update neovim config

--- neovim/.config/nvim/lua/core/health.lua
+++ neovim/.config/nvim/lua/core/health.lua
@@ -1,6 +1,6 @@
 --[[
 --
--- This file is not required for your own configuration,
+- -This file is not required for your own configuration,
 -- but helps people determine if their system is setup correctly.
 --
 --]]
---
+++ neovim/.config/nvim/lua/core/init.lua
@@ -0,0 +1,7 @@
+require("core.set")
+require("core.keymaps")
+require("core.lazy_init")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
