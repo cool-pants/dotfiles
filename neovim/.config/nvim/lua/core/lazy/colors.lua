3fa60e6a14ea60f9039180886a9778a22aa1cd59
cool-pants <muban.sg@gmail.com>
2024-05-15 20:43:51 +0530

    ref: change theme

--- neovim/.config/nvim/lua/core/lazy/colors.lua
+++ neovim/.config/nvim/lua/core/lazy/colors.lua
@@ -1,42 +1,44 @@
 function ColorMyPencils(color)
-    color = color or "gruvbox"
+    color = color or "kanagawa"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
 return {
-    "ellisonleao/gruvbox.nvim",
+    -- "ellisonleao/gruvbox.nvim",
+--    "nyoom-engineering/oxocarbon.nvim",
+    "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
         -- Default options:
-        require("gruvbox").setup({
-            terminal_colors = true, -- add neovim terminal colors
-            undercurl = true,
-            underline = true,
-            bold = true,
-            italic = {
-                strings = true,
-                emphasis = true,
-                comments = true,
-                operators = false,
-                folds = true,
-            },
-            strikethrough = true,
-            invert_selection = false,
-            invert_signs = false,
-            invert_tabline = false,
-            invert_intend_guides = false,
-            inverse = true, -- invert background for search, diffs, statuslines and errors
-            contrast = "", -- can be "hard", "soft" or empty string
-            palette_overrides = {},
-            overrides = {},
-            dim_inactive = false,
-            transparent_mode = false,
-        })
-        vim.cmd("colorscheme gruvbox")
+        -- require("oxocarbon").setup({
+        --     terminal_colors = true, -- add neovim terminal colors
+        --     undercurl = true,
+        --     underline = true,
+        --     bold = true,
+        --     italic = {
+        --         strings = true,
+        --         emphasis = true,
+        --         comments = true,
+        --         operators = false,
+        --         folds = true,
+        --     },
+        --     strikethrough = true,
+        --     invert_selection = false,
+        --     invert_signs = false,
+        --     invert_tabline = false,
+        --     invert_intend_guides = false,
+        --     inverse = true, -- invert background for search, diffs, statuslines and errors
+        --     contrast = "", -- can be "hard", "soft" or empty string
+        --     palette_overrides = {},
+        --     overrides = {},
+        --     dim_inactive = false,
+        --     transparent_mode = false,
+        -- })
+        vim.cmd("colorscheme kanagawa")
         ColorMyPencils()
     end
 }
