1cecdcffd288e7d88826e29997d0f2b08f303c40
cool-pants <muban.sg@gmail.com>
2024-05-17 13:54:56 +0530

    nvim fix: small settings changes

--- neovim/.config/nvim/lua/core/lazy_init.lua
+++ neovim/.config/nvim/lua/core/lazy_init.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = "core.lazy",
-	change_detection = { notify = true },
+	change_detection = { notify = false },
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
--- neovim/.config/nvim/lua/core/set.lua
+++ neovim/.config/nvim/lua/core/set.lua
@@ -35,7 +35,7 @@ vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 500
+vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -59,8 +59,11 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.smartindent = true
+
 vim.opt.wrap = false
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.termguicolors = true
