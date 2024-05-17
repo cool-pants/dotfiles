f59556c018872ed7b944092aaa43f0f6e9f914b8
cool-pants <muban.sg@gmail.com>
2024-05-17 12:40:07 +0530

    feat: add neorg and luarocks for notes

---
+++ neovim/.config/nvim/lua/core/lazy/luarocks.lua
@@ -0,0 +1,7 @@
+return {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = function ()
+        require("luarocks-nvim").setup({})
+    end
+}
---
+++ neovim/.config/nvim/lua/core/lazy/neorg.lua
@@ -0,0 +1,8 @@
+return {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    -- put any other flags you wanted to pass to lazy here!
+    config = function()
+        require("neorg").setup()
+    end,
+}
